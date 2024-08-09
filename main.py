93df4928c8721a4e4468714faa61974c27dfd943
chrisfurlong03 <chrisfurlong03@gmail.com>
2024-08-09 14:36:07 -0500

    chore: Update subproject commit reference

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+#!/usr/bin/python
 import vercel_blob
-from flask import Flask, request, jsonify, send_file, render_template, redirect
+from flask import Flask, request, jsonify
 import os
 import json
 import subprocess
 import dotenv
+from waitress import serve
 
 app = Flask(__name__)
-UPLOAD_DIRECTORY = "/project/workspace/OLMT/metdata_tools/site"
+UPLOAD_DIRECTORY = "/usr/src/elm_olmt_server/OLMT/metdata_tools/site"
 dotenv.load_dotenv()
 
 
@@ -17,16 +19,17 @@ def upload():
 
     data = request.json
     json_file_path = os.path.join(
-        UPLOAD_DIRECTORY, 'processed_ameriflux_data.json')
+        UPLOAD_DIRECTORY, "processed_ameriflux_data.json")
 
     # Write JSON data to a file
     with open(json_file_path, 'w') as json_file:
         json.dump(data, json_file)
 
+    # Check for the query parameter to set the time_step
+    time_step = 48 if request.args.get('aflux') is not None else 24
+
     # Construct absolute paths
-    venv_python = os.path.join("/OLMT/metdata_tools/site", 'bin', 'python')
-    script_path = os.path.join(UPLOAD_DIRECTORY, 'data_to_elmbypass.py')
-    result = subprocess.run([venv_python, script_path],
+    result = subprocess.run(["python","data_to_elmbypass.py",str(time_step)], shell=True,
                             cwd=UPLOAD_DIRECTORY, capture_output=True, text=True)
 
     # Check if the script ran successfully
@@ -35,7 +38,7 @@ def upload():
 
     # Wait for the file to be created
     output_file_path = os.path.join(
-        UPLOAD_DIRECTORY, '1x1pt_MAR-NE/all_hourly.nc')
+        UPLOAD_DIRECTORY, "1x1pt_MAR-NE/all_hourly.nc")
 
     if not os.path.exists(output_file_path):
         return jsonify({"error": "Output file not found"}), 500
@@ -46,40 +49,35 @@ def upload():
     print(result)
     return result
 
-
-@app.route('/upload-json', methods=['POST'])
-def upload_json():
+@app.route('/aflux', methods=['GET'])
+def aflux():
     if not os.path.exists(UPLOAD_DIRECTORY):
         os.makedirs(UPLOAD_DIRECTORY)
 
-    data = request.json
-    json_file_path = os.path.join(
-        UPLOAD_DIRECTORY, 'processed_ameriflux_data.json')
+    os.chdir(UPLOAD_DIRECTORY)
 
-    # Write JSON data to a file
-    with open(json_file_path, 'w') as json_file:
-        json.dump(data, json_file)
+    site = request.args.get("site")
+    start_year = request.args.get("start_year")
+    end_year = request.args.get("end_year")
+    command = [f"Rscript ameriflux_tool.R {str(site)} {str(start_year)} {str(end_year)}"]
 
-    # Construct absolute paths
-    venv_python = os.path.join("/OLMT/metdata_tools/site", 'bin', 'python')
-    script_path = os.path.join(UPLOAD_DIRECTORY, 'data_to_elmbypass.py')
-    result = subprocess.run([venv_python, script_path],
-                            cwd=UPLOAD_DIRECTORY, capture_output=True, text=True)
+    # Run the R script
+    result = subprocess.run(command, shell=True, cwd=UPLOAD_DIRECTORY, capture_output=True, text=True)
 
-    # Check if the script ran successfully
     if result.returncode != 0:
         return jsonify({"error": "Script execution failed", "details": result.stderr}), 500
-
-    # Wait for the file to be created
-    output_file_path = os.path.join(
-        UPLOAD_DIRECTORY, '1x1pt_MAR-NE/all_hourly.nc')
-
-    if not os.path.exists(output_file_path):
+    
+    data_path = os.path.join(
+        UPLOAD_DIRECTORY, "data_to_upload.json")
+    
+    if not os.path.exists(data_path):
         return jsonify({"error": "Output file not found"}), 500
-
-    # Send the file as a response
-    return send_file(output_file_path, mimetype='application/x-netcdf', as_attachment=True, download_name='all_hourly.nc')
-
+    
+    with open(data_path, 'r') as json_file:
+        data = json.load(json_file)
+    
+    # Return the JSON data as a response
+    return jsonify(data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    serve(app, listen='*:80')
